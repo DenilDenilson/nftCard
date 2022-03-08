a413cf6cac1ada4febf9ca7a74c0ae645a1713ef
DenilDenilson <jordanpreseleccion@gmail.com>
2022-03-07 22:56:14 -0500

    Parece que salio bien el webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,42 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+
+
 module.exports = {
-  entry: 'index.js',
+  entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: 'index_bundle.js',
   },
-  plugins: [new HtmlWebpackPlugin()],
+
+  plugins: [
+      new HtmlWebpackPlugin({
+        template: './public/index.html',
+        filename: './index.html',
+      }),
+
+      new MiniCssExtractPlugin()
+
+
+    ],
+
+    module: {
+        rules: [
+          {
+            test: /\.css$/i,
+            use: [MiniCssExtractPlugin.loader, "css-loader"],
+          },
+        ],
+      },
+
+    optimization: {
+        minimizer: [
+          // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
+          // `...`,
+          new CssMinimizerPlugin(),
+        ],
+      },
 };
